ebe0f4a9577deb0a7ae394c20e575ef5e61c8ac5
mydmk7874 <mydmk7874@gmail.com>
2023-01-31 22:20:07 +0900

    약수의 개수와 덧셈 설명 추가

--- src/prac77884_number_of_divisor/Prac77884.java
+++ src/prac77884_number_of_divisor/Prac77884.java
@@ -1,5 +1,8 @@
 package prac77884_number_of_divisor;
 
+//약수의 개수와 덧셈
+//두 정수 left와 right가 매개변수로 주어집니다. left부터 right까지의 모든 수들 중에서,
+// 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
 public class Prac77884 {
     public static void main(String[] args) {
         Solution s= new Solution();
