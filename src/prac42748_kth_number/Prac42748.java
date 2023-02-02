6736d4ea874845df00a8e2d2532ae966b8ca327c
mydmk7874 <mydmk7874@gmail.com>
2023-02-02 19:39:18 +0900

    K번째 수 답안

---
+++ src/prac42748_kth_number/Prac42748.java
@@ -0,0 +1,31 @@
+package prac42748_kth_number;
+
+import java.util.Arrays;
+
+//K번째수
+// 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+// 배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때,
+// commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록
+// solution 함수를 작성해주세요.
+public class Prac42748 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        System.out.println(Arrays.toString(s.solution(array,commands)));
+    }
+}
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+
+        return answer;
+    }
+}
