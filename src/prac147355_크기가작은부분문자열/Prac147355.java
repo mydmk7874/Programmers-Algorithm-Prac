7770e2e9b913d686e2a91acf16a2c557f4f27029
mydmk7874 <mydmk7874@gmail.com>
2023-08-01 21:39:50 +0900

    크기가 작은 부분 문자열 답안

---
+++ src/prac147355_크기가작은부분문자열/Prac147355.java
@@ -0,0 +1,39 @@
+package prac147355_크기가작은부분문자열;
+
+//크기가 작은 부분문자열
+//숫자로 이루어진 문자열 t와 p가 주어질 때, t에서 p와 길이가 같은 부분문자열 중에서,
+// 이 부분문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 횟수를 return하는
+// 함수 solution을 완성하세요.
+//
+//예를 들어, t="3141592"이고 p="271" 인 경우, t의 길이가 3인 부분 문자열은 314, 141, 415, 159, 592입니다.
+// 이 문자열이 나타내는 수 중 271보다 작거나 같은 수는 141, 159 2개 입니다.
+public class Prac147355 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String t= "500220839878";
+        String p = "7";
+        System.out.println(s.solution(t,p));
+    }
+}
+
+
+
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        String[] tt = t.split("");
+
+        for (int i = 0; i < t.length() - p.length(); i++) {
+            String aa = "";
+            for (int j = i; j < p.length(); j++) {
+                aa += tt[j];
+            }
+            int a = Integer.parseInt(aa);
+            if(a < Integer.parseInt(p)) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
