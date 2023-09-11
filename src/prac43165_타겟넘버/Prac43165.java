3762a1c93503a26cb55073e2cdf13ee56dec8b6a
mydmk7874 <mydmk7874@gmail.com>
2023-09-11 11:36:50 +0900

    타겟 넘버 답안

--- src/prac43165_타겟넘버/Prac43165.java
+++ src/prac43165_타겟넘버/Prac43165.java
@@ -41,4 +41,20 @@ class Solution {
 }
 
 //너무 원시적으로 푼거같다.
-
+//다른답안
+//class Solution {
+//    public int solution(int[] numbers, int target) {
+//        int answer = 0;
+//        answer = dfs(numbers, 0, 0, target);
+//        return answer;
+//    }
+//    int dfs(int[] numbers, int n, int sum, int target) {
+//        if(n == numbers.length) {
+//            if(sum == target) {
+//                return 1;
+//            }
+//            return 0;
+//        }
+//        return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target);
+//    }
+//}
